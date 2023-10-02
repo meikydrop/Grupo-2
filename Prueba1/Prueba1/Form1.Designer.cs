410414726786c09d6eb31ca87c4642a0a0635301
Yohanse20 <104366226+Yohanse20@users.noreply.github.com>
2023-10-02 14:49:11 -0400

    Add files via upload

---
+++ Prueba1/Prueba1/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Prueba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            dungeonControlBox1 = new ReaLTaiizor.Controls.DungeonControlBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(dungeonControlBox1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(2302, 114);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(879, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(322, 41);
+            label1.TabIndex = 2;
+            label1.Text = "Nombre de la empresa";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(14, 13);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(119, 80);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // dungeonControlBox1
+            // 
+            dungeonControlBox1.BackColor = Color.Transparent;
+            dungeonControlBox1.DefaultLocation = true;
+            dungeonControlBox1.EnableMaximize = true;
+            dungeonControlBox1.EnableMinimize = true;
+            dungeonControlBox1.Font = new Font("Marlett", 7F, FontStyle.Regular, GraphicsUnit.Point);
+            dungeonControlBox1.Location = new Point(2120, 54);
+            dungeonControlBox1.Name = "dungeonControlBox1";
+            dungeonControlBox1.Size = new Size(64, 22);
+            dungeonControlBox1.TabIndex = 1;
+            dungeonControlBox1.Text = "dungeonControlBox1";
+            dungeonControlBox1.Click += dungeonControlBox1_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(2302, 1408);
+            Controls.Add(panel1);
+            Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private ReaLTaiizor.Controls.MetroControlBox metroControlBox1;
+        private ReaLTaiizor.Controls.DungeonControlBox dungeonControlBox1;
+    }
+}
---
+++ Prueba1/Prueba1/Form1.cs
@@ -0,0 +1,45 @@
+namespace Prueba1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void nightControlBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dungeonControlBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void controlBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroControlBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nightControlBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void thunderControlBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dungeonControlBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
